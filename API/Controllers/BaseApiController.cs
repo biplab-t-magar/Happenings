42c7b4f88e6c3e37860cda08ac0f61a479f6c805
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2021-05-26 20:38:06 -0400

    Error handling implemented on both client and server side

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,9 +1,10 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace API
-{   
+{
     [ApiController]
     [Route("api/[controller]")]
 
@@ -13,6 +14,17 @@ namespace API
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
-   
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null)
+                return NotFound();
+            if (result.IsSuccess && result.Value != null)
+                return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
+
     }
 }
--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Domain 
+namespace Domain
 {
     public class Activity 
     {
