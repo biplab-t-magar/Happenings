26e175acd22d4c27d466ad9197c735dd4ebb8b59
Biplab Thapa Magar <biplab.t.magar@gmail.com>
2021-05-28 17:54:53 -0400

    Added attendance feature between users and activities. Authorization requirement for editing activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,7 +9,6 @@ namespace API
 {
     public class ActivitiesController : BaseApiController
     {
-        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetActivities()
         {
@@ -28,6 +27,7 @@ namespace API
             return HandleResult(await Mediator.Send(new Create.Command{Activity = activity}));
         }
 
+        [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, Activity activity)
         {
@@ -35,10 +35,17 @@ namespace API
             return HandleResult(await Mediator.Send(new Edit.Command{Activity = activity}));
         }
 
+        [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            return HandleResult(await Mediator.Send(new UpdateAttendance.Command{Id = id}));
         }
     }
 }
